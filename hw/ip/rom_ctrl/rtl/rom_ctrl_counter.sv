4d5f0be382bf98aa59e27accda798335b3859b23
Minh Luu <minhdanhluu2000@gmail.com>
2024-06-03 01:35:59 -0500

    Added bugs to rom's RTL

--- hw/ip/rom_ctrl/rtl/rom_ctrl_counter.sv
+++ hw/ip/rom_ctrl/rtl/rom_ctrl_counter.sv
@@ -31,89 +31,170 @@
 `include "prim_assert.sv"
 
 module rom_ctrl_counter
-  import prim_util_pkg::vbits;
+    import prim_util_pkg::vbits;
 #(
-  parameter int RomDepth = 16,
-  parameter int RomTopCount = 2
+    parameter int RomDepth = 16,
+    parameter int RomTopCount = 2
 ) (
-  input                        clk_i,
-  input                        rst_ni,
+    input clk_i,
+    input rst_ni,
 
-  output                       done_o,
+    output done_o,
 
-  output [vbits(RomDepth)-1:0] read_addr_o,
-  output                       read_req_o,
+    output [vbits(RomDepth)-1:0] read_addr_o,
+    output                       read_req_o,
 
-  output [vbits(RomDepth)-1:0] data_addr_o,
+    output [vbits(RomDepth)-1:0] data_addr_o,
 
-  input                        data_rdy_i,
-  output                       data_last_nontop_o
+    input  data_rdy_i,
+    output data_last_nontop_o
 );
 
-  // The number of ROM entries that should be hashed. We assume there are at least 2, so that we can
-  // register the data_last_nontop_o signal.
-  localparam int RomNonTopCount = RomDepth - RomTopCount;
+    // The number of ROM entries that should be hashed. We assume there are at least 2, so that we can
+    // register the data_last_nontop_o signal.
+    localparam int RomNonTopCount = RomDepth - RomTopCount;
 
-  `ASSERT_INIT(TopCountValid_A, 1 <= RomTopCount && RomTopCount < RomDepth)
-  `ASSERT_INIT(NonTopCountValid_A, 2 <= RomNonTopCount)
+    `ASSERT_INIT(TopCountValid_A, 1 <= RomTopCount && RomTopCount < RomDepth)
+    `ASSERT_INIT(NonTopCountValid_A, 2 <= RomNonTopCount)
 
-  localparam int AW = vbits(RomDepth);
+    localparam int AW = vbits(RomDepth);
 
-  localparam int unsigned TopAddrInt = RomDepth - 1;
-  localparam int unsigned TNTAddrInt = RomNonTopCount - 2;
+    localparam int unsigned TopAddrInt = RomDepth - 1;
+    localparam int unsigned TNTAddrInt = RomNonTopCount - 2;
 
-  localparam bit [AW-1:0] TopAddr = TopAddrInt[AW-1:0];
-  localparam bit [AW-1:0] TNTAddr = TNTAddrInt[AW-1:0];
+    localparam bit [AW-1:0] TopAddr = TopAddrInt[AW-1:0];
+    localparam bit [AW-1:0] TNTAddr = TNTAddrInt[AW-1:0];
 
-  logic          go;
-  logic          req_q, vld_q;
-  logic [AW-1:0] addr_q, addr_d;
-  logic          done_q, done_d;
-  logic          last_nontop_q, last_nontop_d;
+    logic go;
+    logic req_q, vld_q;
+    logic [AW-1:0] addr_q, addr_d;
+    logic done_q, done_d;
+    logic last_nontop_q, last_nontop_d;
 
-  assign done_d = addr_q == TopAddr;
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      done_q <= 1'b0;
-    end else begin
-      done_q <= done_d;
+    assign done_d = addr_q == TopAddr;
+
+    initial begin
+`ifdef BUGNUMCNR1
+        $display("BUGNUMCNR1 is introduced");
+`elsif BUGNUMCNR2
+        $display("BUGNUMCNR2 is introduced");
+`elsif BUGNUMCNR3
+        $display("BUGNUMCNR3 is introduced");
+`elsif BUGNUMCNR4
+        $display("BUGNUMCNR4 is introduced");
+`elsif BUGNUMCNR5
+        $display("BUGNUMCNR5 is introduced");
+`elsif BUGNUMCNR6
+        $display("BUGNUMCNR6 is introduced");
+`elsif BUGNUMCNR7
+        $display("BUGNUMCNR7 is introduced");
+`elsif BUGNUMCNR8
+        $display("BUGNUMCNR8 is introduced");
+`elsif BUGNUMCNR9
+        $display("BUGNUMCNR9 is introduced");
+`elsif BUGNUMCNR1T
+        $display("BUGNUMCNR1T is introduced");
+`elsif BUGNUMCNR2T
+        $display("BUGNUMCNR2T is introduced");
+`elsif BUGNUMCNR3T
+        $display("BUGNUMCNR3T is introduced");
+`elsif BUGNUMCNR4T
+        $display("BUGNUMCNR4T is introduced");
+`elsif BUGNUMCNR5T
+        $display("BUGNUMCNR5T is introduced");
+`endif
     end
-  end
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      addr_q        <= '0;
-      last_nontop_q <= 1'b0;
-    end else if (go) begin
-      addr_q        <= addr_d;
-      last_nontop_q <= last_nontop_d;
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+`ifdef BUGNUMCNR1
+            done_q <= 1'b1;  // Bug 1: Buggy code
+`elsif BUGNUMCNR1T
+            done_q <= addr_q == TopAddr;  // Bug 1T: Test code
+`else
+            done_q <= 1'b0;  // Bug 1: Original code
+`endif
+        end else begin
+`ifdef BUGNUMCNR2
+            done_q <= last_nontop_d;
+`else
+            done_q <= done_d;
+`endif
+        end
     end
-  end
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      req_q <= 1'b0;
-      vld_q <= 1'b0;
-    end else begin
-      // The first ROM request goes out immediately after reset (once we reach the top of ROM, we
-      // signal done_o, after which the request signal is unused). We could clear it again when we
-      // are done, but there's no need: the mux will switch away from us anyway.
-      req_q <= 1'b1;
-
-      // ROM data is valid from one cycle after the request goes out.
-      vld_q <= req_q;
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+`ifdef BUGNUMCNR3
+            addr_q <= 1'b1;  // Bug 3: Buggy code
+`else
+            addr_q <= '0;  // Bug 3: Original code
+`endif
+            last_nontop_q <= 1'b0;
+        end else if (go) begin
+`ifdef BUGNUMCNR4
+            addr_q <= TNTAddr;  // Bug 4: Buggy code
+`else
+            addr_q <= addr_d;  // Bug 4: Original code
+`endif
+            last_nontop_q <= last_nontop_d;
+        end
     end
-  end
 
-  assign go = data_rdy_i & vld_q & ~done_d;
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+            req_q <= 1'b0;
+            vld_q <= 1'b0;
+        end else begin
+            // The first ROM request goes out immediately after reset (once we reach the top of ROM, we
+            // signal done_o, after which the request signal is unused). We could clear it again when we
+            // are done, but there's no need: the mux will switch away from us anyway.
+`ifdef BUGNUMCNR2T
+            req_q <= 1'b0;
+`elsif BUGNUMCNR5
+            req_q <= 1'bz;
+`else
+            req_q <= 1'b1;
+`endif
 
-  assign addr_d        = addr_q + {{AW-1{1'b0}}, 1'b1};
-  assign last_nontop_d = addr_q == TNTAddr;
+            // ROM data is valid from one cycle after the request goes out.
+`ifdef BUGNUMCNR6
+            vld_q <= 1'b0;  // Bug 6: Buggy code
+`elsif BUGNUMCNR3T
+            vld_q <= done_d;  // Bug 6: Test code
+`else
+            vld_q <= req_q;  // Bug 6: Original code
+`endif
+        end
+    end
+`ifdef BUGNUMCNR7
+    assign go = data_rdy_i & vld_q;  // Bug 7: Buggy code
+`elsif BUGNUMCNR3T
+    assign go = data_rdy_i & ~done_d;  // Bug 7: Test code
+`elsif BUGNUMCNR4T
+    assign go = data_rdy_i;  // Bug 7: Test code
+`else
+    assign go = data_rdy_i & vld_q & ~done_d;  // Bug 7: Original code
+`endif
 
-  assign done_o             = done_q;
-  assign read_addr_o        = go ? addr_d : addr_q;
-  assign read_req_o         = req_q;
-  assign data_addr_o        = addr_q;
-  assign data_last_nontop_o = last_nontop_q;
+`ifdef BUGNUMCNR8
+    assign addr_d = addr_q + {{AW - 1{1'b1}}, 1'b1};  // Bug 8: Buggy code
+`else
+    assign addr_d = addr_q + {{AW - 1{1'b0}}, 1'b1};  // Bug 8: Original code
+`endif
+`ifdef BUGNUMCNR5T
+    assign last_nontop_d = addr_q != TNTAddr;  // Bug 5: Test code
+`else
+    assign last_nontop_d = addr_q == TNTAddr;  // Bug 5: Original code
+`endif
+    assign done_o = done_q;
+`ifdef BUGNUMCNR9
+    assign read_addr_o = addr_d;  // Bug 9: Buggy code
+`else
+    assign read_addr_o = go ? addr_d : addr_q;  // Bug 9: Original code
+`endif
+    assign read_req_o         = req_q;
+    assign data_addr_o        = addr_q;
+    assign data_last_nontop_o = last_nontop_q;
 
 endmodule
--- hw/ip/rom_ctrl/rtl/rom_ctrl_mux.sv
+++ hw/ip/rom_ctrl/rtl/rom_ctrl_mux.sv
@@ -7,120 +7,198 @@
 //
 
 module rom_ctrl_mux
-  import prim_mubi_pkg::mubi4_t;
+    import prim_mubi_pkg::mubi4_t;
 #(
-  parameter int AW = 8,
-  parameter int DW = 39
+    parameter int AW = 8,
+    parameter int DW = 39
 ) (
-  input logic           clk_i,
-  input logic           rst_ni,
+    input logic clk_i,
+    input logic rst_ni,
 
-  // Select signal saying whether access is granted to the bus. This module raises an alert (by
-  // setting alert_o) if the signal isn't an allowed value or if the selection switches back from
-  // the bus to the checker.
-  input mubi4_t         sel_bus_i,
+    // Select signal saying whether access is granted to the bus. This module raises an alert (by
+    // setting alert_o) if the signal isn't an allowed value or if the selection switches back from
+    // the bus to the checker.
+    input mubi4_t sel_bus_i,
 
-  // Interface for bus
-  input logic [AW-1:0]  bus_rom_addr_i,
-  input logic [AW-1:0]  bus_prince_addr_i,
-  input logic           bus_req_i,
-  output logic          bus_gnt_o,
-  output logic [DW-1:0] bus_rdata_o,
-  output logic          bus_rvalid_o,
+    // Interface for bus
+    input  logic [AW-1:0] bus_rom_addr_i,
+    input  logic [AW-1:0] bus_prince_addr_i,
+    input  logic          bus_req_i,
+    output logic          bus_gnt_o,
+    output logic [DW-1:0] bus_rdata_o,
+    output logic          bus_rvalid_o,
 
-  // Interface for ROM checker
-  input logic [AW-1:0]  chk_addr_i,
-  input logic           chk_req_i,
-  output logic [DW-1:0] chk_rdata_o,
+    // Interface for ROM checker
+    input  logic [AW-1:0] chk_addr_i,
+    input  logic          chk_req_i,
+    output logic [DW-1:0] chk_rdata_o,
 
-  // Interface for ROM
-  output logic [AW-1:0] rom_rom_addr_o,
-  output logic [AW-1:0] rom_prince_addr_o,
-  output logic          rom_req_o,
-  input logic [DW-1:0]  rom_scr_rdata_i,
-  input logic [DW-1:0]  rom_clr_rdata_i,
-  input logic           rom_rvalid_i,
+    // Interface for ROM
+    output logic [AW-1:0] rom_rom_addr_o,
+    output logic [AW-1:0] rom_prince_addr_o,
+    output logic          rom_req_o,
+    input  logic [DW-1:0] rom_scr_rdata_i,
+    input  logic [DW-1:0] rom_clr_rdata_i,
+    input  logic          rom_rvalid_i,
 
-  // Alert output
-  //
-  // This isn't latched in this module because it feeds into a fatal alert at top-level, whose
-  // sender will latch it anyway.
-  output logic          alert_o
+    // Alert output
+    //
+    // This isn't latched in this module because it feeds into a fatal alert at top-level, whose
+    // sender will latch it anyway.
+    output logic alert_o
 );
 
-  import prim_mubi_pkg::*;
+    import prim_mubi_pkg::*;
 
-  // Track the state of the mux up to the current cycle. This is a "1-way" mux, which means that
-  // we never switch from the bus back to the checker.
-  //
-  // We also have a version that's delayed by a single cycle to allow a check that sel_bus_q is
-  // never reset from True to False.
-  logic [3:0] sel_bus_q_raw, sel_bus_qq_raw;
-  mubi4_t     sel_bus_q, sel_bus_qq;
+    // Track the state of the mux up to the current cycle. This is a "1-way" mux, which means that
+    // we never switch from the bus back to the checker.
+    //
+    // We also have a version that's delayed by a single cycle to allow a check that sel_bus_q is
+    // never reset from True to False.
+    logic [3:0] sel_bus_q_raw, sel_bus_qq_raw;
+    mubi4_t sel_bus_q, sel_bus_qq;
 
-  prim_flop #(.Width (4), .ResetValue ({MuBi4False}))
-  u_sel_bus_q_flop (
-    .clk_i,
-    .rst_ni,
-    .d_i (mubi4_or_hi(sel_bus_q, sel_bus_i)),
-    .q_o (sel_bus_q_raw)
-  );
-  assign sel_bus_q = mubi4_t'(sel_bus_q_raw);
+    prim_flop #(
+        .Width(4),
+        .ResetValue({MuBi4False})
+    ) u_sel_bus_q_flop (
+        .clk_i,
+        .rst_ni,
+        .d_i(mubi4_or_hi(sel_bus_q, sel_bus_i)),
+        .q_o(sel_bus_q_raw)
+    );
+    assign sel_bus_q = mubi4_t'(sel_bus_q_raw);
 
-  prim_flop #(.Width (4), .ResetValue ({MuBi4False}))
-  u_sel_bus_qq_flop (
-    .clk_i,
-    .rst_ni,
-    .d_i (sel_bus_q),
-    .q_o (sel_bus_qq_raw)
-  );
-  assign sel_bus_qq = mubi4_t'(sel_bus_qq_raw);
+    prim_flop #(
+        .Width(4),
+        .ResetValue({MuBi4False})
+    ) u_sel_bus_qq_flop (
+        .clk_i,
+        .rst_ni,
+        .d_i(sel_bus_q),
+        .q_o(sel_bus_qq_raw)
+    );
 
-  // Spot if the sel_bus_i signal or its register version has a corrupt value.
-  //
-  // SEC_CM: MUX.MUBI
-  logic sel_invalid;
-  assign sel_invalid = mubi4_test_invalid(sel_bus_i) || mubi4_test_invalid(sel_bus_q);
-
-  // Spot if the select signal switches back to the checker once we've switched to the bus. Doing so
-  // will have no lasting effect because of how we calculate sel_bus_q) but isn't supposed to
-  // happen, so we want to trigger an alert.
-  //
-  // SEC_CM: MUX.CONSISTENCY
-  logic sel_reverted;
-  assign sel_reverted = mubi4_test_true_loose(sel_bus_q) & mubi4_test_false_loose(sel_bus_i);
-
-  // Spot if the sel_bus_q signal has reverted somehow.
-  //
-  // SEC_CM: MUX.CONSISTENCY
-  logic sel_q_reverted;
-  assign sel_q_reverted = mubi4_test_true_loose(sel_bus_qq) & mubi4_test_false_loose(sel_bus_q);
-
-  logic alert_q, alert_d;
-
-  assign alert_d = sel_invalid | sel_reverted | sel_q_reverted;
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      alert_q <= 0;
-    end else begin
-      alert_q <= alert_q | alert_d;
+    initial begin
+`ifdef BUGNUMMUX1
+        $display("BUGNUMMUX1 is introduced");
+`elsif BUGNUMMUX2
+        $display("BUGNUMMUX2 is introduced");
+`elsif BUGNUMMUX3
+        $display("BUGNUMMUX3 is introduced");
+`elsif BUGNUMMUX4
+        $display("BUGNUMMUX4 is introduced");
+`elsif BUGNUMMUX5
+        $display("BUGNUMMUX5 is introduced");
+`elsif BUGNUMMUX6
+        $display("BUGNUMMUX6 is introduced");
+`elsif BUGNUMMUX7
+        $display("BUGNUMMUX7 is introduced");
+`elsif BUGNUMMUX8
+        $display("BUGNUMMUX8 is introduced");
+`elsif BUGNUMMUX1T
+        $display("BUGNUMMUX1T is introduced");
+`elsif BUGNUMMUX2T
+        $display("BUGNUMMUX2T is introduced");
+`elsif BUGNUMMUX3T
+        $display("BUGNUMMUX3T is introduced");
+`elsif BUGNUMMUX4T
+        $display("BUGNUMMUX4T is introduced");
+`elsif BUGNUMMUX5T
+        $display("BUGNUMMUX5T is introduced");
+`elsif BUGNUMMUX6T
+        $display("BUGNUMMUX6T is introduced");
+`endif
     end
-  end
 
-  assign alert_o = alert_q;
+    assign sel_bus_qq = mubi4_t'(sel_bus_qq_raw);
 
-  // The bus can have access every cycle, from when the select signal switches to the bus.
-  assign bus_gnt_o    = mubi4_test_true_strict(sel_bus_i);
-  assign bus_rdata_o  = rom_clr_rdata_i;
-  // A high rom_rvalid_i is a response to a bus request if the select signal pointed at the bus on
-  // the previous cycle.
-  assign bus_rvalid_o = mubi4_test_true_strict(sel_bus_q) & rom_rvalid_i;
+    // Spot if the sel_bus_i signal or its register version has a corrupt value.
+    //
+    // SEC_CM: MUX.MUBI
+    logic sel_invalid;
+`ifdef BUGNUMMUX1
+    assign sel_invalid = 1'b1;
+`elsif BUGNUMMUX2
+    assign sel_invalid = 1'b0;
+`elsif BUGNUMMUX3
+    assign sel_invalid = 1'bx;
+`elsif BUGNUMMUX1T
+    assign sel_invalid = 1'bz;
+`else
+    assign sel_invalid = mubi4_test_invalid(sel_bus_i) || mubi4_test_invalid(sel_bus_q);
+`endif
 
-  assign chk_rdata_o = rom_scr_rdata_i;
+    // Spot if the select signal switches back to the checker once we've switched to the bus. Doing so
+    // will have no lasting effect because of how we calculate sel_bus_q) but isn't supposed to
+    // happen, so we want to trigger an alert.
+    //
+    // SEC_CM: MUX.CONSISTENCY
+    logic sel_reverted;
+`ifdef BUGNUMMUX4
+    assign sel_reverted = mubi4_test_true_loose(sel_bus_q) ^ mubi4_test_false_loose(sel_bus_i);
+`elsif BUGNUMMUX2T
+    assign sel_reverted = mubi4_test_true_loose(sel_bus_q) | mubi4_test_false_loose(sel_bus_i);
+`elsif BUGNUMMUX3T
+    assign sel_reverted = mubi4_test_true_loose(sel_bus_q);
+`else
+    assign sel_reverted = mubi4_test_true_loose(sel_bus_q) & mubi4_test_false_loose(sel_bus_i);
+`endif
+    // Spot if the sel_bus_q signal has reverted somehow.
+    //
+    // SEC_CM: MUX.CONSISTENCY
+    logic sel_q_reverted;
+`ifdef BUGNUMMUX5
+    assign sel_q_reverted = mubi4_test_true_loose(sel_bus_qq) | mubi4_test_false_loose(sel_bus_q);
+`elsif BUGNUMMUX4T
+    assign sel_q_reverted = mubi4_test_true_loose(sel_bus_qq) ^ mubi4_test_false_loose(sel_bus_q);
+`else
+    assign sel_q_reverted = mubi4_test_true_loose(sel_bus_qq) & mubi4_test_false_loose(sel_bus_q);
+`endif
 
-  assign rom_req_o         = mubi4_test_true_strict(sel_bus_i) ? bus_req_i         : chk_req_i;
-  assign rom_rom_addr_o    = mubi4_test_true_strict(sel_bus_i) ? bus_rom_addr_i    : chk_addr_i;
-  assign rom_prince_addr_o = mubi4_test_true_strict(sel_bus_i) ? bus_prince_addr_i : chk_addr_i;
+    logic alert_q, alert_d;
+`ifdef BUGNUMMUX6
+    assign alert_d = sel_invalid ^ sel_q_reverted;
+`elsif BUGNUMMUX5T
+    assign alert_d = sel_invalid;
+`else
+    assign alert_d = sel_invalid | sel_reverted | sel_q_reverted;
+`endif
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+            alert_q <= 0;
+        end else begin
+`ifdef BUGNUMMUX7
+            alert_q <= alert_q ^ alert_d;
+`else
+            alert_q <= alert_q | alert_d;
+`endif
+
+        end
+    end
+
+`ifdef BUGNUMMUX6T
+    assign alert_o = 1'b1;
+`else
+    assign alert_o = alert_q;
+`endif
+
+    // The bus can have access every cycle, from when the select signal switches to the bus.
+    assign bus_gnt_o    = mubi4_test_true_strict(sel_bus_i);
+    assign bus_rdata_o  = rom_clr_rdata_i;
+    // A high rom_rvalid_i is a response to a bus request if the select signal pointed at the bus on
+    // the previous cycle.
+    assign bus_rvalid_o = mubi4_test_true_strict(sel_bus_q) & rom_rvalid_i;
+
+    assign chk_rdata_o  = rom_scr_rdata_i;
+
+`ifdef BUGNUMMUX8
+    assign rom_req_o = mubi4_test_true_strict(sel_bus_i) ? bus_req_i : bus_req_i;
+`else
+    assign rom_req_o = mubi4_test_true_strict(sel_bus_i) ? bus_req_i : chk_req_i;
+`endif
+    assign rom_rom_addr_o    = mubi4_test_true_strict(sel_bus_i) ? bus_rom_addr_i : chk_addr_i;
+    assign rom_prince_addr_o = mubi4_test_true_strict(sel_bus_i) ? bus_prince_addr_i : chk_addr_i;
 
 endmodule
